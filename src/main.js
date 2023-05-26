aa87b52afea6414bcdbe7a0e05cdb9c5b78822a2
Manuel Esteban Tapia Sarmiento <manuel.tapia.9318@hotmail.com>
2023-05-26 00:23:32 -0500

    seguir con el video 12

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ async function getTrendingMoviesPreview() {
   const data = await res.json();
   
   const movies = data.results;
+
+  trendingMoviesPreviewList.innerHTML = "";
+
   movies.forEach(movie => {
-    const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList')
     const movieContainer = document.createElement('div');
     movieContainer.classList.add('movie-container');
     const movieImg = document.createElement('img');
@@ -23,15 +25,18 @@ async function getTrendingMoviesPreview() {
 async function getcategoriesPreview() {
   const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
   const data = await res.json();
-  
+
+  categoriesPreviewList.innerHTML = "";
   const categories = data.genres;
   categories.forEach(categori => {
-    const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list')
     const categoryContainer = document.createElement('div');
     categoryContainer.classList.add('category-container');
     const categoryTitle = document.createElement('h3');
     categoryTitle.classList.add('category-title');
     categoryTitle.setAttribute('id', `id${categori.id}`);
+    categoryTitle.addEventListener('click',()=>{
+      location.hash = `#category=${categori.id}-${categori.name}`
+    })
     const categoryTitleText = document.createTextNode(categori.name)
     
     categoryTitle.appendChild(categoryTitleText);
@@ -41,8 +46,28 @@ async function getcategoriesPreview() {
   });
 }
 
+async function getMoviesByCategory() {
+  const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${id}`);
+  const data = await res.json();
+  
+  const movies = data.results;
 
+  genericSection.innerHTML = "";
+  movies.forEach(movie => {
+    const movieContainer = document.createElement('div');
+    movieContainer.classList.add('movie-container');
+    const movieImg = document.createElement('img');
+    movieImg.classList.add('movie-img');
+    movieImg.setAttribute('alt', movie.title);
+    movieImg.setAttribute('src', `https://image.tmdb.org/t/p/w300/${movie.backdrop_path}`);
+    
+    movieContainer.appendChild(movieImg);
+    genericSection.appendChild(movieContainer);
+  });
+  
+}
 
+getMoviesByCategory()
 
 
 
